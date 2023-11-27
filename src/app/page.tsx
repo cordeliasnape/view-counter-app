6f53fc3ce61aa09a9a0e0144d08457af01cfe7f1
Cordelia Snape <snapecordelia@gmail.com>
2023-11-27 11:20:39 +0000

    added page views count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  await sql`INSERT INTO ViewsTable (views) SELECT 0 WHERE NOT EXISTS(SELECT * FROM ViewsTable)`;
+  await sql`UPDATE ViewsTable SET views = views + 1`;
+  const result = await sql`SELECT views from ViewsTable`;
+
+  console.log(result);
+
   return (
     <div>
       <p>Root route</p>
+      <p>This has been viewed {result.rows[0].views} times</p>
     </div>
   );
 }
